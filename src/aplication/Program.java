4152e3dd2228a72279ff5ec0b590ed3e8f969a6f
davidmozart <davidmozart01@gmail.com>
2021-08-26 23:17:09 -0300

    Acrescentada mensagem Bom dia

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -41,6 +41,8 @@ public class Program {
 		System.out.println("y1: "+sdf1.format(y1));
 		System.out.println("y2: "+sdf3.format(y2));
 		System.out.println("y3: "+sdf3.format(y3));
+		
+		System.out.println("Bom Dia!");
 
 	}
 
